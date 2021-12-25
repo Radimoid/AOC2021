40f5890e19beda685abbe204318e2cfe8a1dee39
radimoid <r.langer@seznam.cz>
2021-12-25 07:29:43 +0100

    day25

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace AOC2021
     {
         static void Main(string[] args)
         {
-            var solver = new Day24();
+            var solver = new Day25();
             solver.PartOne();
             solver.PartTwo();
         }
